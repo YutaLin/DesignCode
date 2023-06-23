4581d568c470a71b8c2280c3a06224a60b28a6a3
Yuta <yuta@avc.fund>
2023-06-23 14:16:31 +0800

    SwiftUI app for iOS 15 init

---
+++ DesignCodeiOS15/DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Yuta on 2023/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
